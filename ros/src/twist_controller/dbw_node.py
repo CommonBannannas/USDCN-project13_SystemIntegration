d35fc587a57cb89674cf920bbb4d582c63176ed0
Daniel Fernandez <almondhive@gmail.com>
2018-07-01 14:02:16 -0500

    change cte

--- ros/src/twist_controller/dbw_node.py
+++ ros/src/twist_controller/dbw_node.py
@@ -11,42 +11,6 @@ from styx_msgs.msg import Lane
 from twist_controller import Controller
 
 
-def get_xy_from_waypoints(waypoints):
-
-    return list(map(lambda waypoint: [waypoint.pose.pose.position.x, waypoint.pose.pose.position.y], waypoints))
-
-
-def get_cross_track_error(final_waypoints, current_pose):
-
-    origin = final_waypoints[0].pose.pose.position
-
-    waypoints_matrix = get_xy_from_waypoints(final_waypoints)
-
-
-    shifted_matrix = waypoints_matrix - np.array([origin.x, origin.y])
-
-
-    offset = 15
-    angle = np.arctan2(shifted_matrix[offset, 1], shifted_matrix[offset, 0])
-    rotation_matrix = np.array([
-            [np.cos(angle), -np.sin(angle)],
-            [np.sin(angle), np.cos(angle)]
-        ])
-
-    rotated_matrix = np.dot(shifted_matrix, rotation_matrix)
-
-
-    degree = 3
-    coefficients = np.polyfit(rotated_matrix[:, 0], rotated_matrix[:, 1], degree)
-
-    # Transform the current pose of the car to be in the car's coordinate system
-    shifted_pose = np.array([current_pose.pose.position.x - origin.x, current_pose.pose.position.y - origin.y])
-    rotated_pose = np.dot(shifted_pose, rotation_matrix)
-
-    expected_y_value = np.polyval(coefficients, rotated_pose[0])
-    actual_y_value = rotated_pose[1]
-
-    return expected_y_value - actual_y_value
 
 
 class DBWNode(object):
@@ -105,6 +69,43 @@ class DBWNode(object):
         self.pose_sub = rospy.Subscriber('/current_pose', PoseStamped, self.current_pose_cb, queue_size=1)
         self.loop()
 
+    def get_xy_from_waypoints(self, waypoints):
+
+        return list(map(lambda waypoint: [waypoint.pose.pose.position.x, waypoint.pose.pose.position.y], waypoints))
+
+
+    def get_cross_track_error(self, final_waypoints, current_pose):
+
+        origin = final_waypoints[0].pose.pose.position
+
+        waypoints_matrix = self.get_xy_from_waypoints(final_waypoints)
+
+
+        shifted_matrix = waypoints_matrix - np.array([origin.x, origin.y])
+
+
+        offset = 15
+        angle = np.arctan2(shifted_matrix[offset, 1], shifted_matrix[offset, 0])
+        rotation_matrix = np.array([
+                [np.cos(angle), -np.sin(angle)],
+                [np.sin(angle), np.cos(angle)]
+            ])
+
+        rotated_matrix = np.dot(shifted_matrix, rotation_matrix)
+
+
+        degree = 3
+        coefficients = np.polyfit(rotated_matrix[:, 0], rotated_matrix[:, 1], degree)
+
+        # Transform the current pose of the car to be in the car's coordinate system
+        shifted_pose = np.array([current_pose.pose.position.x - origin.x, current_pose.pose.position.y - origin.y])
+        rotated_pose = np.dot(shifted_pose, rotation_matrix)
+
+        expected_y_value = np.polyval(coefficients, rotated_pose[0])
+        actual_y_value = rotated_pose[1]
+
+        return expected_y_value - actual_y_value
+
     def loop(self):
         # rate 50 Hz
         rate = rospy.Rate(50)
@@ -122,7 +123,7 @@ class DBWNode(object):
                 target_linear_velocity = self.proposed_velocity.twist.linear.x
 
                 target_angular_velocity = self.proposed_velocity.twist.angular.z
-                cross_track_error = cte_calculator.get_cross_track_error(self.final_waypoints, self.current_pose)
+                cross_track_error = self.get_cross_track_error(self.final_waypoints, self.current_pose)
 
                 throttle, brake, steering = self.controller.control(target_linear_velocity,
                                                                     target_angular_velocity,
