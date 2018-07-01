edf3161bf34c2f82d13bc10ae13814bd9ff2dcc1
Sergio Santoyo <sergiosantoyo.92@gmail.com>
2018-07-01 12:23:34 -0500

    fixed bugs

--- ros/src/twist_controller/twist_controller.py
+++ ros/src/twist_controller/twist_controller.py
@@ -20,17 +20,13 @@ class Controller(object):
         self.steer_ratio = kwargs['steer_ratio']
         self.max_lat_accel = kwargs['max_lat_accel']
         self.max_steer_angle = kwargs['max_steer_angle']
-        min_speed = 0 # TODO: What is a good value for min speed?
+        min_speed = 0
         self.s_lpf = LowPassFilter(tau = 0.5, ts = 1)
 
-        #self.linear_pid = PID(kp=0.2, ki=0.005, kd=0.1, mn=decel_limit, mx=0.5 * accel_limit)
         self.linear_pid = PID(kp=0.8, ki=0, kd=0.05, mn=self.decel_limit, mx=0.5 * self.accel_limit)
         self.yaw_controller = YawController(self.wheel_base, self.steer_ratio, min_speed, self.max_lat_accel, self.max_steer_angle)
         self.steering_pid = PID(kp=0.15, ki=0.001, kd=1, mn=-self.max_steer_angle, mx=self.max_steer_angle)
 
-    def reset(self):
-        self.linear_pid.reset()
-        self.steering_pid.reset()
 
     def control(self, proposed_linear_velocity, proposed_angular_velocity, current_linear_velocity, cross_track_error, duration_in_seconds):
         linear_velocity_error = proposed_linear_velocity - current_linear_velocity
@@ -42,13 +38,18 @@ class Controller(object):
 
         if(throttle < 0):
             deceleration = abs(throttle)
-            brake = (self.vehicle_mass + self.fuel_capacity * GAS_DENSITY) * self.wheel_radius * deceleration if deceleration > self.brake_deadband else 0.
-            throttle = 0
+            brake = (self.vehicle_mass + self.fuel_capacity * GAS_DENSITY) * self.wheel_radius * deceleration if deceleration > self.brake_deadband else 1.
+            throttle = 0.0
 
         predictive_steering = self.yaw_controller.get_steering(proposed_linear_velocity, proposed_angular_velocity, current_linear_velocity)
         corrective_steering = self.steering_pid.step(cross_track_error, duration_in_seconds)
-        steering = predictive_steering + corrective_steering * 0.25
+        steering = predictive_steering + corrective_steering * 0.33
 
         steering = self.s_lpf.filt(steering)
 
         return throttle, brake, steering
+
+
+    def reset(self):
+        self.linear_pid.reset()
+        self.steering_pid.reset()
